6d89cb920a4ac9a3087d134bf3eca52f7cdde17b
bivanalhar <bivan.alzacky@gmail.com>
2018-05-16 17:26:14 +0900

    GUI User Interface : Basic Classifier Complete

--- gui_debugging.py
+++ gui_debugging.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import mnist_classifier
 
 img_bool = True
 med_bool = True
@@ -16,20 +17,20 @@ class Window(Frame):
 	#creating the window through the function init_window
 	def init_window(self):
 		#adding the operated button within the window
-		self.master.title("Debugging Interface")
-		self.master.geometry("600x800")
+		self.master.title("Medical Information")
+		self.master.geometry("600x650")
 		self.master.configure(background = "lightblue")
 
 		self.pack(fill = BOTH, expand = 1)
 
 		quitbutton = Button(self, text = "Quit", command = self.client_exit)
-		quitbutton.place(x = 550, y = 770)
+		quitbutton.place(x = 550, y = 620)
 
 		imagebutton = Button(self, text = "Show Patient Info", command = self.showhideImage)
-		imagebutton.place(x = 0, y = 0)
+		imagebutton.place(x = 0, y = 10)
 
 		medbutton = Button(self, text = "Show Medical Info", command = self.showhideMedImage)
-		medbutton.place(x = 300, y = 0)
+		medbutton.place(x = 300, y = 10)
 
 		statbutton = Button(self, text = "Show Statistics Info", command = self.showhideStatImage)
 		statbutton.place(x = 0, y = 200)
@@ -54,7 +55,7 @@ class Window(Frame):
 		# menu.add_cascade(label = "Edit", menu = edit)
 
 	###################################################################
-	#BEGIN Button 1: Showing the operation for the patients information
+	#BEGIN Button 1: Showing the operation for the patients info
 	def showhideImage(self):
 		global img_bool
 
@@ -73,7 +74,7 @@ class Window(Frame):
 
 		img = Label(self, image=render)
 		img.image = render
-		img.place(x = 0, y = 30)
+		img.place(x = 0, y = 40)
 
 		img_bool = False
 
@@ -82,11 +83,13 @@ class Window(Frame):
 
 		img.destroy()
 		img_bool = True
-	#END Button 1
+
+	####################################################################
+	########################END Button 1################################
 	####################################################################
 
 	####################################################################
-	#BEGIN Button 2 : Showing the operation for the medical information
+	#BEGIN Button 2 : Showing the operation for the medical info
 	def showhideMedImage(self):
 		global med_bool
 
@@ -105,7 +108,7 @@ class Window(Frame):
 
 		med = Label(self, image=render)
 		med.image = render
-		med.place(x = 300, y = 30)
+		med.place(x = 300, y = 40)
 
 		med_bool = False
 
@@ -114,11 +117,13 @@ class Window(Frame):
 
 		med.destroy()
 		med_bool = True
-	#END Button 2
-	#####################################################################
 
-	#####################################################################
-	#BEGIN Button 3 : Showing the operation for the statistic information
+	####################################################################
+	########################END Button 2################################
+	####################################################################
+
+	####################################################################
+	#BEGIN Button 3 : Showing the operation for the statistic info
 	def showhideStatImage(self):
 		global stat_bool
 
@@ -146,16 +151,35 @@ class Window(Frame):
 
 		stat.destroy()
 		stat_bool = True
-	#END Button 3
-	#####################################################################
 
-	#####################################################################
+	####################################################################
+	########################END Button 3################################
+	####################################################################
+
+	####################################################################
 	#BEGIN Button 4 : Classification Button
 	def classify(self):
-		print("classification not yet defined")
+		result = mnist_classifier.classifier()
 
-	#END Button 4
-	#####################################################################
+		global top
+
+		top = Toplevel()
+		top.title("Classification Result")
+
+		res_text = "Accuracy\n" + str(result)[:5] + "\npercent"  
+
+		msg = Message(top, text = res_text)
+		msg.config(font = ("Arial", 20), justify = CENTER)
+		msg.pack()
+
+		top.geometry("150x150")
+
+		quitchildbut = Button(top, text = "Quit", command = self.child_destroy)
+		quitchildbut.place(x = 50, y = 120)
+
+	####################################################################
+	########################END Button 4################################
+	####################################################################
 
 	# def showText(self):
 	# 	text = Label(self, text = "Collection of Hearts")
@@ -165,6 +189,10 @@ class Window(Frame):
 	#defining the operation for the quit button
 	def client_exit(self):
 		exit()
+
+	def child_destroy(self):
+		global top
+		top.destroy()
 
 root = Tk()
 
--- mnist_classifier.py
+++ mnist_classifier.py
@@ -1,21 +1,41 @@
 import tensorflow as tf
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-#variable to put on the input image and one-hot encoded label
-input_image = tf.placeholder(tf.float32, [None, 784])
-real_label = tf.placeholder(tf.float32, [None, 10])
+tf.logging.set_verbosity(old_v)
 
-#the parameters to be trained : weight and bias
-weight = tf.Variable(tf.zeros([784, 10]))
-bias = tf.Variable(tf.zeros([10]))
+def classifier():
+	from tensorflow.examples.tutorials.mnist import input_data
+	mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
 
-#begin the classification process
-y = tf.nn.softmax(tf.matmul(input_image, weight) + bias)
+	#variable to put on the input image and one-hot encoded label
+	input_image = tf.placeholder(tf.float32, [None, 784])
+	real_label = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(real_label * tf.log(y), reduction_indices = [1]))
+	#the parameters to be trained : weight and bias
+	weight = tf.Variable(tf.zeros([784, 10]))
+	bias = tf.Variable(tf.zeros([10]))
 
-train_step = tf.train.AdamOptimizer(learning_rate = 0.1).minimize(cross_entropy)
-#finish the classification process
+	#begin the classification process
+	y = tf.nn.softmax(tf.matmul(input_image, weight) + bias)
 
+	cross_entropy = tf.reduce_mean(-tf.reduce_sum(real_label * tf.log(y), reduction_indices = [1]))
+
+	train_step = tf.train.AdamOptimizer(learning_rate = 0.01).minimize(cross_entropy)
+	#finish the classification process
+
+	sess = tf.InteractiveSession()
+	tf.global_variables_initializer().run()
+
+	for _ in range(1000):
+		batch_xs, batch_ys = mnist.train.next_batch(100)
+		sess.run(train_step, feed_dict = {input_image : batch_xs, real_label : batch_ys})
+
+	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(real_label, 1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	result = 100 * sess.run(accuracy, feed_dict={input_image: mnist.test.images, \
+		real_label: mnist.test.labels})
+
+	sess.close()
+	return result
