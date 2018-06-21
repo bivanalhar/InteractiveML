0496391c18631702075eb132cd0cfa9990c6d73d
bivanalhar <bivan.alzacky@gmail.com>
2018-06-21 17:30:10 +0900

    GUI User Interface : Imp Words, Prior Prob Embedded

--- init_pickling.py
+++ init_pickling.py
@@ -2,6 +2,9 @@ import pickle
 
 a = [14, 6]
 b = [4, 16]
+c = [13, 8]
+d = [10, 5]
+e = [4, 11]
 
 with open("save_tuple.pickle", "wb") as file:
-	pickle.dump((a, b), file)
+	pickle.dump((a, b, c, d, e), file)
--- main_interface.py
+++ main_interface.py
@@ -1,4 +1,6 @@
 import pickle
+import plotly.plotly as py
+import plotly.graph_objs as go
 from tkinter import *
 from PIL import Image, ImageTk
 
@@ -6,7 +8,7 @@ from PIL import Image, ImageTk
 #of the program in general. this will just contain the
 #2 buttons, one for the doctor and one for the patient
 with open("save_tuple.pickle", "rb") as file:
-	doc, pat = pickle.load(file)
+	doc, pat, barpie, ctext, stext = pickle.load(file)
 
 print("the current probability of doctor seeing complex is {0:.2f}".format(float(doc[0])/(doc[0] + doc[1])))
 print("the current probability of patient seeing simple is {0:.2f}\n".format(float(pat[1])/(pat[0] + pat[1])))
@@ -23,7 +25,7 @@ if init == '1':
 	import init_pickling
 
 	with open("save_tuple.pickle", "rb") as file:
-		doc, pat = pickle.load(file)
+		doc, pat, barpie, ctext, stext = pickle.load(file)
 
 if init == '0' or init == '':
 	print("proceeding")
@@ -48,6 +50,15 @@ class Window(Frame):
 
 		self.init_complex = doc #for the prior probability of the interface outcome
 		self.init_simple = pat #for the prior probability of the interface outcome
+		self.init_barpie = barpie #for the prior probability of the bar/pie stats outcome
+		self.init_ctext = ctext #for the prior probability of the text in complex interface
+		self.init_stext = stext #for the prior probability of the text in simple interface
+
+		self.barpie_bool = None
+		self.ctext_bool = None
+		self.stext_bool = None
+		self.impword_bool = True
+		self.prior_bool = True
 
 		self.init_window()
 
@@ -58,6 +69,11 @@ class Window(Frame):
 		self.stat_bool = True
 		self.text2_bool = True
 
+		self.barpie_bool = None
+		self.ctext_bool = None
+		self.impword_bool = True
+		self.prior_bool = True
+
 		self.is_doctor = 0
 
 	#initializing all the patient-related variables
@@ -65,6 +81,7 @@ class Window(Frame):
 		self.img_bool_patient = True
 		self.med_bool_patient = True
 		self.text_bool = True
+		self.stext_bool = None
 
 		self.is_patient = 0
 
@@ -112,24 +129,28 @@ class Window(Frame):
 		self.top.destroy()
 
 	def checkScreen(self, pd_info, identity):
+		barval = self.init_barpie[0] / float(self.init_barpie[0] + self.init_barpie[1])
+		ctextval = self.init_ctext[0] / float(self.init_ctext[0] + self.init_ctext[1])
+		stextval = self.init_stext[0] / float(self.init_stext[0] + self.init_stext[1])
+
 		if pd_info == 0:
 			probability = self.init_complex[0] / float(self.init_complex[0] + self.init_complex[1])
 			print("Doctor : probability of showing complex is {0:.2f}".format(probability))
 
 			if probability > 0.5:
-				self.showComplex(id_ = identity)
+				self.showComplex(id_ = identity, barval = barval, ctextval = ctextval, stextval = stextval)
 			else:
-				self.showSimple(id_ = identity)
+				self.showSimple(id_ = identity, barval = barval, ctextval = ctextval, stextval = stextval)
 		else:
 			probability = self.init_simple[1] / float(self.init_simple[0] + self.init_simple[1])
 			print("Patient : probability of showing simple is {0:.2f}".format(probability))
 
 			if probability > 0.5:
-				self.showSimple(id_ = identity)
+				self.showSimple(id_ = identity, barval = barval, ctextval = ctextval, stextval = stextval)
 			else:
-				self.showComplex(id_ = identity)
+				self.showComplex(id_ = identity, barval = barval, ctextval = ctextval, stextval = stextval)
 
-	def showComplex(self, id_ = None):
+	def showComplex(self, id_ = None, barval = None, ctextval = None, stextval = None):
 		top = self.top_complex = Toplevel(bg = "lightblue")
 		top.title("Complex Explanation Interface")
 
@@ -194,19 +215,31 @@ class Window(Frame):
 		self.med.image = render
 		self.med.place(x = 800, y = 30)
 
-		statbutton = Button(top, text = "Show Statistics Info", command = lambda: self.showhideStatImage(id_ = id_), bg = "sandybrown")
+		statbutton = Button(top, text = "Show Stats Info", command = lambda: self.showhideStatImage(id_ = id_, barval = barval))
 		statbutton.place(x = 0, y = 250)
 
-		switchbutton = Button(top, text = "Switch to Simple Interface", command = lambda: self.switchSimple(id_ = id_))
-		switchbutton.place(x = 0, y = 600)
+		switchstatbutton = Button(top, text = "Switch Bar/Pie", command = lambda: self.switchbarpie(id_ = id_))
+		switchstatbutton.place(x = 150, y = 250)
 
-		textbutton = Button(top, text = "Show Text Info", command = lambda: self.showhideTextImageComplex(id_ = id_))
-		textbutton.place(x = 500, y = 250)
+		switchbutton = Button(top, text = "Switch to Simple Interface", command = lambda: self.switchSimple(id_ = id_, barval = barval, ctextval = ctextval, stextval = stextval), width = 35)
+		switchbutton.place(x = 0, y = 570)
 
-		quitbutton = Button(top, text = "Quit Complex", command = self.quitComplex)
-		quitbutton.place(x = 500, y = 600)
+		textbutton = Button(top, text = "Show Text Info", command = lambda: self.showhideTextImageComplex(id_ = id_, ctextval = ctextval))
+		textbutton.place(x = 325, y = 250)
 
-	def showSimple(self, id_ = None):
+		switchtextbutton = Button(top, text = "Switch Comp/Simp", command = lambda: self.switchcomsimp(id_ = id_))
+		switchtextbutton.place(x = 475, y = 250)
+
+		impwordbutton = Button(top, text = "Important Words", command = lambda: self.importantWords(id_ = id_))
+		impwordbutton.place(x = 675, y = 250)
+
+		priorbutton = Button(top, text = "Prior Probability", command = self.PriorProbability)
+		priorbutton.place(x = 825, y = 250)
+
+		quitbutton = Button(top, text = "Quit Complex", command = self.quitComplex, width = 35)
+		quitbutton.place(x = 0, y = 600)
+
+	def showSimple(self, id_ = None, barval = None, ctextval = None, stextval = None):
 		top = self.top_simple = Toplevel(bg = "wheat")
 		top.title("Simple Explanation Interface")
 
@@ -271,10 +304,13 @@ class Window(Frame):
 		self.med.image = render
 		self.med.place(x = 550, y = 20)
 
-		textbutton = Button(top, text = "Show Text Info", command = lambda: self.showhideTextImage(id_ = id_))
+		textbutton = Button(top, text = "Show Text Info", command = lambda: self.showhideTextImage(id_ = id_, stextval = stextval))
 		textbutton.place(x = 0, y = 180)
 
-		switchbutton = Button(top, text = "Switch to Complex Interface", command = lambda: self.switchComplex(id_ = id_))
+		switchtextbutton = Button(top, text = "Switch Simp/Comp", command = lambda: self.switchsimcomp(id_ = id_))
+		switchtextbutton.place(x = 350, y = 180)
+
+		switchbutton = Button(top, text = "Switch to Complex Interface", command = lambda: self.switchComplex(id_ = id_, barval = barval, ctextval = ctextval, stextval = stextval))
 		switchbutton.place(x = 0, y = 500)
 
 		quitbutton = Button(top, text = "Quit Complex", command = self.quitSimple)
@@ -282,21 +318,26 @@ class Window(Frame):
 
 	####################################################################
 	#BEGIN Button 3a : Showing the operation for the statistic info
-	def showhideStatImage(self, id_ = None):
+	def showhideStatImage(self, id_ = None, barval = None):
 		if self.stat_bool:
-			self.showStatImage(id_ = id_)
+			if barval > 0.5:
+				self.showStatImage(id_ = id_, barpie = 0)
+				self.barpie_bool = True
+			else:
+				self.showStatImage(id_ = id_, barpie = 1)
+				self.barpie_bool = False
 		else:
 			self.hideStatImage()
+			self.barpie_bool = None
 
 	#defining the operation to open the Image
-	def showStatImage(self, id_ = None):
-		statinfo = Image.open(str(id_) + "_statinfo.jpg")
-		width, height = statinfo.size
+	def showStatImage(self, id_ = None, barpie = None):
+		if barpie == 0:
+			statinfo = Image.open(str(id_) + "_statinfo.jpg")
+		else:
+			statinfo = Image.open(str(id_) + "_pieinfo.jpg")
 
-		width = int(width * 0.8)
-		height = int(height * 0.83)
-
-		statinfo = statinfo.resize((width, height), Image.ANTIALIAS)
+		statinfo = statinfo.resize((310, 200), Image.ANTIALIAS)
 		render = ImageTk.PhotoImage(statinfo)
 
 		self.stat = Label(self.top_complex, image=render)
@@ -308,26 +349,56 @@ class Window(Frame):
 	def hideStatImage(self):
 		self.stat.destroy()
 		self.stat_bool = True
+
+	def switchbarpie(self, id_ = None):
+		if self.barpie_bool == None:
+			print("statistics image not yet shown")
+
+		elif self.barpie_bool:
+			self.stat.destroy()
+			self.showStatImage(id_ = id_, barpie = 1)
+			self.init_barpie[1] += 1
+			self.barpie_bool = False
+
+		else:
+			self.stat.destroy()
+			self.showStatImage(id_ = id_, barpie = 0)
+			self.init_barpie[0] += 1
+			self.barpie_bool = True
+
+		with open("save_tuple.pickle", "wb") as file:
+			pickle.dump((self.init_complex, self.init_simple, self.init_barpie, self.init_ctext, self.init_stext), file)
 	####################################################################
 	########################END Button 3a###############################
 	####################################################################
 
 	####################################################################
 	#BEGIN Button 3b : Showing the operation for the statistic info
-	def showhideTextImage(self, id_ = None):
+	def showhideTextImage(self, id_ = None, stextval = None):
 		if self.text_bool:
-			self.showTextImage(id_ = id_)
+			if stextval > 0.5:
+				self.showTextImage(id_ = id_, simcomp = 1)
+				self.stext_bool = True
+			else:
+				self.showTextImage(id_ = id_, simcomp = 0)
+				self.stext_bool = False
 		else:
 			self.hideTextImage()
+			self.stext_bool = None
 
 	#defining the operation to open the Image
-	def showTextImage(self, id_ = None):
-		self.text = Text(self.top_simple, height = 10, width = 75, font = 12)
+	def showTextImage(self, id_ = None, simcomp = None):
+		self.text = Text(self.top_simple, height = 12, width = 85, font = ("Helvetica", 11))
 
 		self.text.pack()
 
 		self.text.place(x = 0, y = 210)
-		file_open = open(str(id_) + "_text_patient.txt", 'r')
+
+		if simcomp == 0:
+			file_open = open(str(id_) + "_text_patient.txt", 'r')
+		else:
+			file_open = open(str(id_) + "_textinfo.txt", "r")
+
 		file_line = file_open.readlines()
 
 		result = ''
@@ -343,26 +414,56 @@ class Window(Frame):
 	def hideTextImage(self):
 		self.text.destroy()
 		self.text_bool = True
+
+	def switchsimcomp(self, id_ = None):
+		if self.stext_bool == None:
+			print("text complex interface image not yet shown")
+
+		elif self.stext_bool:
+			self.text.destroy()
+			self.showTextImage(id_ = id_, simcomp = 0)
+			self.init_stext[1] += 1
+			self.stext_bool = False
+
+		else:
+			self.text.destroy()
+			self.showTextImage(id_ = id_, simcomp = 1)
+			self.init_stext[0] += 1
+			self.stext_bool = True
+
+		with open("save_tuple.pickle", "wb") as file:
+			pickle.dump((self.init_complex, self.init_simple, self.init_barpie, self.init_ctext, self.init_stext), file)
 	####################################################################
 	########################END Button 3b###############################
 	####################################################################
 
 	####################################################################
 	#BEGIN Button 4 : Showing the operation for the text info complex
-	def showhideTextImageComplex(self, id_ = None):
+	def showhideTextImageComplex(self, id_ = None, ctextval = None):
 		if self.text2_bool:
-			self.showTextImageComplex(id_ = id_)
+			if ctextval > 0.5:
+				self.showTextImageComplex(id_ = id_, comsimp = 0)
+				self.ctext_bool = True
+			else:
+				self.showTextImageComplex(id_ = id_, comsimp = 1)
+				self.ctext_bool = False
 		else:
 			self.hideTextImageComplex()
+			self.ctext_bool = None
 
 	#defining the operation to open the Image
-	def showTextImageComplex(self, id_ = None):
-		self.text2 = Text(self.top_complex, height = 15, width = 45, font = 12)
+	def showTextImageComplex(self, id_ = None, comsimp = None):
+		self.text2 = Text(self.top_complex, height = 17, width = 45, font = ("Helvetica", 10))
 
 		self.text2.pack()
 
-		self.text2.place(x = 500, y = 280)
-		file_open = open(str(id_) + "_textinfo.txt", 'r')
+		self.text2.place(x = 325, y = 280)
+
+		if comsimp == 0:
+			file_open = open(str(id_) + "_textinfo.txt", 'r')
+		else:
+			file_open = open(str(id_) + "_text_patient.txt", "r")
+
 		file_line = file_open.readlines()
 
 		result = ''
@@ -377,12 +478,109 @@ class Window(Frame):
 	def hideTextImageComplex(self):
 		self.text2.destroy()
 		self.text2_bool = True
+
+	def switchcomsimp(self, id_ = None):
+		if self.ctext_bool == None:
+			print("text complex interface image not yet shown")
+
+		elif self.ctext_bool:
+			self.text2.destroy()
+			self.showTextImageComplex(id_ = id_, comsimp = 1)
+			self.init_ctext[1] += 1
+			self.ctext_bool = False
+
+		else:
+			self.text2.destroy()
+			self.showTextImageComplex(id_ = id_, comsimp = 0)
+			self.init_ctext[0] += 1
+			self.ctext_bool = True
+
+		with open("save_tuple.pickle", "wb") as file:
+			pickle.dump((self.init_complex, self.init_simple, self.init_barpie, self.init_ctext, self.init_stext), file)
 	####################################################################
 	########################END Button 4################################
 	####################################################################
 
-	# ####################################################################
-	# #BEGIN Button 5 : Showing the operation for switching and quitting
+	####################################################################
+	#BEGIN Button 5 : Showing the operation for the important words
+	def importantWords(self, id_ = None):
+		if self.impword_bool:
+			self.showImportantWords(id_ = id_)
+		else:
+			self.hideImportantWords()
+
+	#defining the operation to open the Image
+	def showImportantWords(self, id_ = None):
+		self.impword = Text(self.top_complex, height = 6, width = 35, font = ("Helvetica", 11))
+		self.impword.pack()
+		self.impword.place(x = 675, y = 280)
+
+		file_open = open(str(id_) + "_impwords.txt", "r")
+		file_line = file_open.readlines()
+
+		result = ''
+
+		for line in file_line:
+			result += line
+
+		self.impword.insert(END, result)
+		self.impword.config(state = DISABLED)
+
+		self.impword_bool = False
+
+	def hideImportantWords(self):
+		self.impword.destroy()
+		self.impword_bool = True
+	####################################################################
+	########################END Button 5################################
+	####################################################################
+
+	####################################################################
+	#BEGIN Button 6 : Showing the operation for the prior probability
+	def PriorProbability(self):
+		if self.prior_bool:
+			self.showPriorProbability()
+		else:
+			self.hidePriorProbability()
+
+	#defining the operation to open the Image
+	def showPriorProbability(self):
+		comp_val = self.init_complex[0] / float(self.init_complex[0] + self.init_complex[1])
+		simp_val = self.init_simple[1] / float(self.init_simple[0] + self.init_simple[1])
+		barval = self.init_barpie[0] / float(self.init_barpie[0] + self.init_barpie[1])
+		ctextval = self.init_ctext[0] / float(self.init_ctext[0] + self.init_ctext[1])
+		stextval = self.init_stext[0] / float(self.init_stext[0] + self.init_stext[1])
+		
+		data = [go.Bar(
+					x = [comp_val, simp_val, barval, ctextval, stextval],
+					y = ["Doctor Showing Complex", "Patient Showing Simple", "Showing Bar-Pie Chart", "Doctor Showing Complex Text", "Patient Showing Simple Text"],
+					orientation = 'h',
+					width = 0.6
+		)]
+		layout = go.Layout(title = "Prior Probabilities", width = 800, height = 640)
+		fig = go.Figure(data = data, layout = layout)
+
+		py.image.save_as(fig, filename = "prior-chart.jpeg")
+
+		prior_info = Image.open("prior-chart.jpeg")
+		prior_info = prior_info.resize((285, 200), Image.ANTIALIAS)
+		render = ImageTk.PhotoImage(prior_info)
+
+		self.prior = Label(self.top_complex, image=render)
+		self.prior.image = render
+		self.prior.place(x = 675, y = 420)
+
+		self.prior_bool = False
+
+	def hidePriorProbability(self):
+		self.prior.destroy()
+		self.prior_bool = True
+	####################################################################
+	########################END Button 6################################
+	####################################################################
+
+	####################################################################
+	#BEGIN Button 7 : Showing the operation for switching and quitting
 	def quitComplex(self):
 		self.is_doctor = 0
 		self.top_complex.destroy()
@@ -395,34 +593,34 @@ class Window(Frame):
 
 		self.init_simple_var()
 
-	def switchComplex(self, id_ = None):
+	def switchComplex(self, id_ = None, barval = None, ctextval = None, stextval = None):
 		self.quitSimple()
 
 		self.init_complex[0] += 1
 		self.init_simple[0] += 1
 
 		with open("save_tuple.pickle", "wb") as file:
-			pickle.dump((self.init_complex, self.init_simple), file)
+			pickle.dump((self.init_complex, self.init_simple, self.init_barpie, self.init_ctext, self.init_stext), file)
 
 		self.init_complex_var()
 		
-		self.showComplex(id_ = id_)
+		self.showComplex(id_ = id_, barval = barval, ctextval = ctextval, stextval = stextval)
 
-	def switchSimple(self, id_ = None):
+	def switchSimple(self, id_ = None, barval = None, ctextval = None, stextval = None):
 		self.quitComplex()
 
 		self.init_complex[1] += 1
 		self.init_simple[1] += 1
 
 		with open("save_tuple.pickle", "wb") as file:
-			pickle.dump((self.init_complex, self.init_simple), file)
+			pickle.dump((self.init_complex, self.init_simple, self.init_barpie, self.init_ctext, self.init_stext), file)
 
 		self.init_simple_var()
 
-		self.showSimple(id_ = id_)
-	# ####################################################################
-	# ########################END Button 4################################
-	# ####################################################################
+		self.showSimple(id_ = id_, barval = barval, ctextval = ctextval, stextval = stextval)
+	####################################################################
+	########################END Button 7################################
+	####################################################################
 
 	#defining the operation for the quit button
 	def client_exit(self):
